a0fd2eb9037265d9a4787e9f5dacd8f172621bdf
Jerry Ablan <jerryablan@dreamfactory.com>
2016-01-10 23:32:04 -0500

    Allow override of header and token so class can be used for non-console API requests.

--- src/Services/InstanceApiClientService.php
+++ src/Services/InstanceApiClientService.php
@@ -26,6 +26,10 @@ class InstanceApiClientService extends BaseService
      * @type string The base resource uri to use to talk to the instance
      */
     protected $resourceUri;
+    /**
+     * @type array The request headers
+     */
+    protected $headers;
 
     //*************************************************************************
     //* Methods
@@ -34,15 +38,22 @@ class InstanceApiClientService extends BaseService
     /**
      * Initialize and set up the transport layer
      *
-     * @param Instance $instance
+     * @param Instance    $instance
+     * @param string|null $token  The token to use instead of automatic one
+     * @param string|null $header The HTTP header to use instead of DFE one
      *
      * @return $this
      */
-    public function connect(Instance $instance)
+    public function connect(Instance $instance, $token = null, $header = null)
     {
         $this->instance = $instance;
-        $this->token = $this->generateToken([$instance->cluster->cluster_id_text, $instance->instance_id_text]);
-        $this->resourceUri = Uri::segment([$instance->getProvisionedEndpoint(), $instance->getResourceUri()], false);
+
+        //  Note trailing slash added...
+        $this->resourceUri = rtrim(Uri::segment([$instance->getProvisionedEndpoint(), $instance->getResourceUri()], false), '/') . '/';
+
+        //  Set up the channel
+        $this->token = $token ?: $this->generateToken([$instance->cluster->cluster_id_text, $instance->instance_id_text]);
+        $this->headers = [$header ?: EnterpriseDefaults::CONSOLE_X_HEADER . ': ' . $this->token,];
 
         return $this;
     }
@@ -56,7 +67,7 @@ class InstanceApiClientService extends BaseService
     {
         try {
             //  Return all system resources
-            $_response = (array)$this->get();
+            $_response = (array)$this->get('/?as_list=true');
 
             return array_get($_response, 'resource', false);
         } catch (\Exception $_ex) {
@@ -82,7 +93,7 @@ class InstanceApiClientService extends BaseService
     public function resource($resource, $id = null)
     {
         try {
-            $_response = (array)$this->get(Uri::segment([$resource, $id], true));
+            $_response = (array)$this->get(Uri::segment([$resource, $id]));
 
             return array_get($_response, 'resource', false);
         } catch (\Exception $_ex) {
@@ -183,23 +194,18 @@ class InstanceApiClientService extends BaseService
      */
     public function call($uri, $payload = [], $options = [], $method = Request::METHOD_POST)
     {
-        $options[CURLOPT_HTTPHEADER] = array_merge(
-            array_get($options, CURLOPT_HTTPHEADER, []),
-            [EnterpriseDefaults::CONSOLE_X_HEADER . ': ' . $this->token]
-        );
+        $options[CURLOPT_HTTPHEADER] = array_merge(array_get($options, CURLOPT_HTTPHEADER, []), $this->headers ?: []);
 
-        if (!empty($payload) && is_array($payload)) {
+        if (!empty($payload) && !is_scalar($payload)) {
             $payload = Json::encode($payload);
-
-            $options[CURLOPT_HTTPHEADER] = array_merge(
-                array_get($options, CURLOPT_HTTPHEADER, []),
-                ['Content-Type: application/json']
-            );
+            $options[CURLOPT_HTTPHEADER] = array_merge(array_get($options, CURLOPT_HTTPHEADER, []), ['Content-Type: application/json']);
         }
 
         try {
-            $_response = Curl::request($method, Uri::segment([$this->resourceUri, $uri], false), $payload, $options);
+            $_response = Curl::request($method, $this->resourceUri . ltrim($uri, ' /'), $payload, $options);
         } catch (\Exception $_ex) {
+            $this->error('[dfe.instance-api-client] ' . $method . ' failure: ' . $_ex->getMessage());
+
             return false;
         }
 
